d60c3686cf388d31209cc6c4753054c7419fc75a
Tung Dao <me@tungdao.com>
2019-01-04 15:24:57 +0700

    initial commit

---
+++ Gateway/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Gateway
+//
+//  Created by Tung Dao on 4/1/2019.
+//  Copyright © 2019 Tung Dao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
