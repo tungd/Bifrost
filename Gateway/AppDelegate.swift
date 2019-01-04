485198890f56812a1a7e990063474b74a51aa4ea
Tung Dao <me@tungdao.com>
2019-01-04 15:44:26 +0700

    add: remove docker icon and main window

--- Gateway/AppDelegate.swift
+++ Gateway/AppDelegate.swift
@@ -11,16 +11,23 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+            button.action = #selector(printQuote(_:))
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    @objc func printQuote(_ sender: Any?) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
 
+        print("\(quoteText) — \(quoteAuthor)")
+    }
 }
-
